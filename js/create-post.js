db6000fae0cff26cee6f909956140b92dcbb0246
Sierra Stevenson <84206234+sierrastevnson@users.noreply.github.com>
2024-03-13 19:30:23 -0700

    Added create-post.js for post creation

---
+++ js/create-post.js
@@ -0,0 +1,64 @@
+let postvalue = document.getElementById("textarea");
+let posttitle = document.getElementById("textareaTitle");
+let successMessage = document.getElementById("successMessage");
+let currentuser = "";
+let uid;
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        if(user.emailVerified) {
+            uid = user.uid
+        } else {
+            window.location.assign("../html/emailVerification.html")
+        }
+    } else {
+        window.location.assign("../html/authentication.html")
+    }
+  });
+
+  firebase.auth().onAuthStateChanged((user) => {
+    currentuser = user
+  })
+
+
+var d = new Date();
+
+
+function createpost() {
+  if (postvalue.value !== "" && posttitle.value != "") {
+    firebase
+      .firestore()
+      .collection("posts")
+      .add({
+        posttitle: posttitle.value,
+        postvalue: postvalue.value,
+        like: 0,
+        dislikes: 0,
+        user: String(currentuser.email).substring(0, String(currentuser.email).indexOf('@')),
+        comments: [],
+        Date: `${d}`
+      })
+      .then((res) => {
+        firebase
+          .firestore()
+          .collection("posts/")
+          .doc(res.id)
+          .update({
+            id: res.id
+          })
+          .then(() => {
+            successMessage.textContent = "Post successfully submitted.";
+            successMessage.style.color = "green";
+            setTimeout(() => {
+              location.reload();
+            }, 500);
+          });
+      });
+  }
+}
+
+const logout = ()=>{
+  firebase.auth().signOut().then(() => {
+    window.location.assign("./authentication.html")
+  })
+}
