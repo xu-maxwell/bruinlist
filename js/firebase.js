f0ee60886304ff30bca95ff4f73eecf86e4399e6
Maxwell Xu <137871969+xu-maxwell@users.noreply.github.com>
2024-03-13 21:26:26 -0700

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,14 @@
+// Grader: Please enter the information provided in final reports.
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
